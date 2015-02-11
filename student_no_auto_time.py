fec8c4d29e0df05298b637d73c88d3b1c96222af
Ryan Manly <ryan.manly@gmail.com>
2015-02-11 16:02:22 -0600

    add weekend auto run and post 15:15 capability per rtrouton, rsaeks, and myself

--- student_no_auto_time.py
+++ student_no_auto_time.py
@@ -1,9 +1,25 @@
 #!/usr/bin/python
 
-import time
-import sys
+from sys import argv
+from time import localtime
 
-no_fly_list = [7, 8, 9, 10, 11, 12, 13, 14]
+no_run_hours = [7, 8, 9, 10, 11, 12, 13, 14, 15]
 
-if time.localtime()[3] in no_fly_list and sys.argv[1] == 'auto':
-    exit(1)
+now = localtime()
+
+(current_weekday, current_hour, current_min) = now[6], now[3], now[4]
+
+try:
+    if argv[1] == 'auto':
+        # allow auto to run on Saturday and Sunday
+        if current_weekday == 5 or current_weekday == 6:
+            exit()
+        # allow times in 15:xx after 15:15
+        elif current_hour == 15 and current_min >= 15:
+            exit()
+        # disallow munki auto run for all other hours in list
+        elif current_hour in no_run_hours:
+            exit(1)
+except IndexError:
+    # somehow we didn't get passed a runtype
+    exit(2) 

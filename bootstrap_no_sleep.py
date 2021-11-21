ce4d99e9dbbe1480fe7349ea4fca4de52805371c
Ryan Manly <ryan.manly@gmail.com>
2021-11-20 19:16:43 -0600

    done playing around

--- bootstrap_no_sleep.py
+++ bootstrap_no_sleep.py
@@ -5,7 +5,9 @@ import sys
 
 try:
     if sys.argv[1] == 'checkandinstallatstartup':
-        cmd = ["/usr/bin/pmset", "-a", "sleep", "x", "displaysleep", "0", "disksleep", "0"]
-        subprocess.run(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        cmd = ["/usr/bin/pmset", "-a", "sleep", "0", "displaysleep", "0", "disksleep", "0"]
+        subprocess.run(cmd, capture_output=True, check=True)
 except IndexError:
     sys.exit("We didn't get passed a munki runtype!") 
+except subprocess.CalledProcessError:
+    sys.exit("pmset failed")
